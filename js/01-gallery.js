ba40def229b7f726dabf9ea9df7c04ffe389f0e3
JuliusB-ui <baraquiel.julius@gmail.com>
2024-04-11 07:59:35 +0800

    attempt 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+let gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  console.log(item);
+  let li = `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`;
+  gallery.innerHTML += li;
+});
