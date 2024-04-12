77f33a23fb001db00f736dd11d3a0779f2b0afda
JuliusB-ui <baraquiel.julius@gmail.com>
2024-04-12 13:45:27 +0800

    for submission

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,9 +4,8 @@ import { galleryItems } from "./gallery-items.js";
 let gallery = document.querySelector(".gallery");
 
 galleryItems.forEach((item) => {
-  console.log(item);
   let li = `<li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="javascript:void(0)">
     <img
       class="gallery__image"
       src="${item.preview}"
@@ -16,4 +15,21 @@ galleryItems.forEach((item) => {
   </a>
 </li>`;
   gallery.innerHTML += li;
+});
+
+gallery.addEventListener("click", (event) => {
+  console.log(event);
+  if (event.target.tagName === "IMG") {
+    let bigImage = event.target.getAttribute("data-source");
+    let instance = basicLightbox.create(`
+    <img src="${bigImage}" width="800" height="600">
+`);
+    instance.show();
+
+    window.addEventListener("keyup", (event) => {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+    });
+  }
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+let galleryContainer = document.querySelector("ul.gallery");
+
+const photosMarkup = createGalleryItem(galleryItems);
+
+function createGalleryItem(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+
+const galleryHander = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
+
+galleryHander.on("show.SimpleLightbox");
